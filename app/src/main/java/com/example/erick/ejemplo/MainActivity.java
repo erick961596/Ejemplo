ef7e258f1fbe31ad8f0e9edd2e4924361cfbe7df
erick961596 <erick.perezrayo54@gmai.com>
2017-02-12 15:06:44 -0600

    mi primer comit

---
+++ app/src/main/java/com/example/erick/ejemplo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.erick.ejemplo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.FacebookSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+
+
+}
